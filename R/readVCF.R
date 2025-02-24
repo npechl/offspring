3ec481de0496eb6b9e6e253180c6bdfc75abfe6d
nikopech <nikosp41@gmail.com>
2025-02-24 12:07:08 +0200

    vcf wrapper input

---
+++ R/readVCF.R
@@ -0,0 +1,27 @@
+
+
+
+
+readVCF <- function(path) {
+    
+    varo <- path |> read.vcfR(verbose = FALSE)
+    
+    varo@fix[, "ID"] <- paste0(
+        varo@fix[, "CHROM"], ":",
+        varo@fix[, "POS"], "_",
+        varo@fix[, "REF"], "_",
+        varo@fix[, "ALT"]
+    )
+    
+    # fix <- varo |> getFIX()
+    # 
+    # fix[, ]
+    
+    varo <- varo |> vcfR2tidy(single_frame = TRUE, verbose = FALSE)
+    varo <- varo$dat |> setDT()
+    
+    trees <- varo[, c("Indiv", "ID", "gt_GT_alleles"), with = FALSE] |>
+        dcast(Indiv ~ ID, value.var = "gt_GT_alleles")
+    
+    return(trees)
+}
